a69fcc8718e9f3281910a476076acb94eeece1ee
Kayden Clark <kaydenclark725@gmail.com>
2020-12-22 20:40:00 -0700

    my clock ticks!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TimeClock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      time: new Date().toLocaleString(),
+      success: null
+    }
+  }
+
+  tick(){
+    this.setState({
+      time: new Date().toLocaleString()
+    })
+  }
+
+  componentDidMount(){
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalID)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>Time Entry</header>
+        <div className="Entry_input">
+          <p> {this.state.time} </p> <br/>
+          <input placeholder='employee ID'/> <br/>
+          <input placeholder='password'/> <br/>
+          <button name='clockIn'>Clock In</button> 
+          <button name='clockOut'>Clock Out</button> <br/>
+          <p>{this.state.success}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TimeClock;
