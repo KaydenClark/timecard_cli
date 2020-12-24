1d95075ee36197293f1af4c0bcdcfb875c91ceb2
Kayden Clark <kaydenclark725@gmail.com>
2020-12-23 17:54:24 -0700

    Moved clockin to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import axios from 'axios'
-import './App.css';
+import './App.css'
+import {
+  base,
+  // API PROD
+} from './components/consts'
 
 class TimeClock extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       time: new Date().toLocaleString(),
-      success: null
+      success: null,
+      employeeID: '',
+      pin: ''
     }
+  }
+
+  postClockInAxios = async () => {
+      console.log(`sending clock in time: ${this.state.time}`)
+      const req = await axios.post(`${base}/clockIn`, {
+          timeIn: this.time
+      })
+      return req
+  }
+
+  submitClockTime = async (e) => {
+      e.preventDefault()
+      const valid = await this.postClockInAxios()
+      if(valid){
+          alert(`Clocked in at ${this.state.time}`)
+      } else {
+          alert(`Invaild entry`)
+      }
+
   }
 
   tick(){
@@ -35,7 +60,7 @@ class TimeClock extends React.Component {
         <div className="Entry_input">
           <p> {this.state.time} </p> <br/>
           <input placeholder='employee ID'/> <br/>
-          <input placeholder='password'/> <br/>
+          <input placeholder='pin'/> <br/>
           <button name='clockIn'>Clock In</button> 
           <button name='clockOut'>Clock Out</button> <br/>
           <p>{this.state.success}</p>
